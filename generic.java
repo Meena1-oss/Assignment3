f5b3863f8fa897bf34ee403290d3af65ed9599bc
Meena1-oss <85676845+Meena1-oss@users.noreply.github.com>
2021-06-18 09:04:16 +0530

    Add files via upload

---
+++ generic.java
@@ -0,0 +1,9 @@
+package collection;
+
+import java.util.*;
+public class generic {
+public static void main(String s[ ]){
+List<String> list=new ArrayList<String>( );
+list.add("one");
+list.add("2");
+System.out.println(list.get(0).length()); }
